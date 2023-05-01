2a3226c59a66d33970fa77f9bb4fc619a667b1ba
simon <simonkhartoum@gmail.com>
2023-05-01 11:55:31 +0300

    completed

---
+++ botsapp/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/Army';
+
+function App() {
+  const botsAPI = 'http://localhost:3000/bots'
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(botsAPI)
+    .then(res => res.json())
+    .then(setBots)
+  }, []);
+
+  function enlistBot(id) {
+    setBots(bots.map(bot => id === bot.id ? {...bot, isEnlisted: true} : bot));
+  }
+  
+
+  function delistBot(id) {
+    setBots(bots.map(bot => id === bot.id ? {...bot, isEnlisted: false} : bot));
+  }
+  
+  function dischargeBot(id) {
+    setBots(bots.filter(bot => bot.id === id ? false : true));
+  }
+
+  return (
+    <div className="App">
+      <YourBotArmy bots={bots.filter(bot => bot.isEnlisted)} handleDischarge={dischargeBot} handleClick={delistBot} />
+      <BotCollection bots= {bots}  handleDischarge={dischargeBot} handleClick={enlistBot}/>
+    </div>
+  );
+}
+
+export default App;
